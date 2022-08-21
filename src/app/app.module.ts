ef20da2ee0358aa6d58c3028fd1847b2580fa9a5
x00Pavel <pavel.yadlouski@gmail.com>
2022-08-21 22:12:05 +0200

    Add components to app.model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    RestaurantComponent,
+    RestaurantListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: RestaurantListComponent },
+    ])
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
