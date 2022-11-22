81236f9d90babbd2b0953ae1a899f28689cc89b1
Pavel Yadlouski <pavel.yadlouski@veeam.com>
2022-11-22 18:06:07 +0100

    Update build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-lunch-web'`, () => {
+  it(`should have as title 'Lunch web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-lunch-web');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:7788'
+  apiUrl: 'http://142.132.165.58:7788/'
 };
